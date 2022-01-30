4f1339b94e54eaa9fa373b6dca8f93be84777072
Pedro Lucas <78425332+PedroLucas63@users.noreply.github.com>
2022-01-30 12:58:13 +0000

    Incremento do conceito de Orientação a Objetos.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-//Escreve um texto no console:
-Console.WriteLine("Hello, word!");;
+//Importação da biblioteca principal do C#
+using System;
+
+//Nomeação do projeto:
+namespace HelloWorld{
+  //Criação da classe principal da aplicação:
+  class App{
+    //Criação da função principal da aplicação:
+    public static void Main(string[] args){
+      //Escreve um texto no console:
+      Console.WriteLine("Hello, word!");;
+    }
+  }
+}
